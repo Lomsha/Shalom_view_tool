be5e9733e2c32c5e59ed064816762d975632bbc4
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-18 08:37:28 +0300

    Shalom Tool initial commit

---
+++ lib/Shalom_view_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "Shalom_view_tool/version"
+require_relative "Shalom_view_tool/renderer"
+
+
+module ShalomViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Shalom_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ShalomViewTool
+  VERSION = "0.1.0"
+end
